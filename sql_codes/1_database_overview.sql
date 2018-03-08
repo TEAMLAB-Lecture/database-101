6b50bda75d82d314de2cbe9a60299bf69459185c
Sungchul Choi <sc82.choi@gachon.ac.kr>
2018-03-08 12:52:03 +0900

    update codes

---
+++ sql_codes/1_database_overview.sql
@@ -0,0 +1,19 @@
+# CMD
+mysql -u root -p
+
+# MySQL
+SHOW databases;
+
+CREATE DATABASE northwind;
+USE northwind;
+show tables;
+
+exit;
+
+# CMD
+mysql -u root -p northwind < Northwind.MySQL5.sql
+mysql -u root -p
+
+# MySQL
+USE northwind;
+show tables;
