368bd5ea1c6cdec632ea8ca22f6a0937ae864242
Sungchul Choi <sc82.choi@gachon.ac.kr>
2019-03-10 18:56:00 +0900

    add overall sampledataset

---
+++ dataset/SampleDatabases/Item19Structure.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE Item19Example;
+
+USE Item19Example;
+
+CREATE TABLE tblPostSales(
+    Product varchar (255), 
+    Jan decimal(15, 2), 
+    Feb decimal(15, 2), 
+    Mar decimal(15, 2), 
+    Apr decimal(15, 2), 
+    May decimal(15, 2), 
+    Jun decimal(15, 2), 
+    Jul decimal(15, 2), 
+    Aug decimal(15, 2), 
+    Sep decimal(15, 2), 
+    Oct decimal(15, 2), 
+    Nov decimal(15, 2), 
+    Decm decimal(15, 2));  
---
+++ dataset/SampleDatabases/Item30StructureAndData.sql
@@ -0,0 +1,17 @@
+-- Create a new database, to make it easier to cleanup afterwards.
+
+CREATE DATABASE Item30Example;
+
+USE Item30Example;
+
+CREATE TABLE Inventory (
+  Color varchar(15) NULL,
+  Dimension  varchar(5) NULL,
+  Quantity int NULL
+);
+
+INSERT INTO Inventory (Color, Dimension, Quantity) VALUES ('Red', 'L', 10);
+INSERT INTO Inventory (Color, Dimension, Quantity) VALUES ('Blue', 'M', 20);
+INSERT INTO Inventory (Color, Dimension, Quantity) VALUES ('Red', 'M', 15);
+INSERT INTO Inventory (Color, Dimension, Quantity) VALUES ('Blue', 'L', 5);
+
